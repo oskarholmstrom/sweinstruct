15de7fa4f009efba363b31179314cc2aad8efe3d
Ehsan <30546099+edoost@users.noreply.github.com>
2023-06-07 11:31:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@ pip install --quiet torch==1.12.1 torchvision torchaudio
 
 pip install --quiet git+https://github.com/huggingface/transformers.git
 
-pip install —-quiet nltk
+pip install --quiet nltk
 
 git clone https://github.com/huggingface/evaluate
 cd evaluate
 pip install -e .
 cd ..
 
-pip install —-quiet sentencepiece
-pip install —-quiet rouge_score
+pip install --quiet sentencepiece
+pip install --quiet rouge_score
 pip install --quiet bitsandbytes
-pip install —-quiet accelerate
-pip install —-quiet scipy
+pip install --quiet accelerate
+pip install --quiet scipy
